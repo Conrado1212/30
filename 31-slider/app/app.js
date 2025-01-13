c6c33bc40459fbae45ca9aa2aebee615673bdb79
Conrado1212 <conrado1212@gmail.com>
2025-01-13 18:27:44 +0100

    simple slider

--- 31-slider/app/app.js
+++ 31-slider/app/app.js
@@ -1,11 +1,19 @@
 
-const slide = document.querySelectorAll(".slides img");
+let slide = document.querySelectorAll(".slides img");
+let slide2 = document.querySelector(".slides");
 
 
 let index = 0;
 
 let intervalId = null;
 document.addEventListener("DOMContentLoaded", initialize);
+
+slide2.addEventListener("wheel", (e) =>{
+    e.preventDefault();
+    slide2.scrollLeft += e.deltaY;
+})
+
+
 function initialize(){
 
     slide.length > 0  ? (slide[index].classList.add('active'), intervalId =  setInterval(next, 4000)) :  false;
@@ -37,4 +45,34 @@ function prev(){
 clearInterval(intervalId);
 index--;
 show(index);
+}
+
+
+let con = document.querySelector('.gallery');
+//mouse wheel scroll to rotate slide
+con.addEventListener("wheel", (e) =>{
+    e.preventDefault();
+    con.scrollLeft += e.deltaY;
+})
+
+function nextBTN(){
+    console.log(con.scrollLeft);
+    if(con.scrollLeft >= 900){
+        con.scrollLeft = 0;
+    }else{
+        con.scrollLeft += 900;
+        console.log(con.scrollLeft);
+    }
+}
+
+function prevBTN(){
+
+    console.log(con.scrollLeft);
+ //   con.scrollLeft -= 900;
+     if(con.scrollLeft <= 0){
+         con.scrollLeft = 900;
+     }else{
+         con.scrollLeft -= 900;
+        console.log(con.scrollLeft);
+     }
 }
