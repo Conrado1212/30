7e847d025528e4fd17bb66a09a850620590b4fb2
Conrado1212 <conrado1212@gmail.com>
2025-01-25 18:55:36 +0100

    table test

--- 32-tableTest/app/app.js
+++ 32-tableTest/app/app.js
@@ -40,31 +40,63 @@ function add(){
 
 }
 function addData(){
-    const inputs1 = document.querySelectorAll('#table1 input[type="text"]');
-    const inputs2 = document.querySelectorAll('#table2 input[type="text"]');
+    const inputs1 = document.querySelectorAll('#table1 td:nth-child(2) input[type="text"]');
+   
+    const inputs2 = document.querySelectorAll('#table2 td:nth-child(2) input[type="text"]');
+
+    
+    const inputs2test = document.getElementById('table1').getElementsByTagName('td');
+    
+    const inputs3test = document.getElementById('table2').getElementsByTagName('td');
+
+    for(let i=0;i<inputs2test.length;i++){
+      let xd = inputs2test[i].getElementsByTagName('input');
+      let xd2 = inputs3test[i] ? inputs3test[i].getElementsByTagName('input') : [];
+     //   console.log(xd);
+       for(let j=0;j<xd.length;j++){
+           xd[j].addEventListener('input', ()=>{
+               console.log(`test ${xd[j].value}`);
+               if(xd2[j]){
+                   xd2[j].value = xd[j].value;
+               }
+           });
+           if(xd2[j]){
+            xd2[j].addEventListener('input',()=>{
+                console.log(`test ${xd2[j].value}`);
+                if(xd[j]){
+                    xd[j].value = xd2[j].value;
+                }
+               });
+           }
+          
+       }
+    }
+
+   
 
 
-    inputs1.forEach((input, index)=>{
-        input.addEventListener('input', ()=>{
-            if(inputs2[index]){
-                console.log(`Synchro ${input.value}`);
-                inputs2[index].value = input.value;
+
+//     inputs1.forEach((input, index)=>{
+//         input.addEventListener('input', ()=>{
+//             if(inputs2[index]){
+//                 console.log(`Synchro ${input.value}`);
+//                 inputs2[index].value = input.value;
                
-            }
+//             }
             
-        })
-    });
+//         })
+//     });
 
-
-    inputs2.forEach((input, index)=>{
-        input.addEventListener('input', ()=>{
-            if(inputs2[index]){
-                console.log(`Synchro ${input.value}`);
-            inputs1[index].value = input.value;
-            }
-        })
-    });
-}
+// //druga tabela do peirszje 
+//     inputs2.forEach((input, index)=>{
+//         input.addEventListener('input', ()=>{
+//             if(inputs2[index]){
+//                 console.log(`Synchro ${input.value}`);
+//             inputs1[index].value = input.value;
+//             }
+//         })
+//     });
+ }
 
 addData();
 console.log(addData());
