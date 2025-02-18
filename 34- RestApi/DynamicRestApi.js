db8626ec81352dd5704acbc38f75ea143cd3bfcb
Conrado1212 <conrado1212@gmail.com>
2025-02-18 19:13:47 +0100

    modal test

---
+++ 34- RestApi/DynamicRestApi.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+const dynamic ={};
+
+const endpoints =[];
+
+let id = 0;
+
+app.post('new', (res,req)=>{
+
+})
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`App working at http://localhost:${port}`);
+})
