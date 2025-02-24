32405ce79692021e53eefdd42666b808063a3bcd
Conrado1212 <conrado1212@gmail.com>
2025-02-24 19:18:53 +0100

    modal test

--- 5 - gallery/app/app.js
+++ 5 - gallery/app/app.js
@@ -1,18 +1,23 @@
 const panel = document.querySelectorAll('.panel');
 console.log(panel);
-let active = null;
+let active = null; //zmiena do checka czy acitve cvzy nie 
 function toggle(){
     console.log('ddddddd');
   
-
+//sprawdzenie czy thsi to active
     if(active !==this){
+        //jezeli nie to sprawdzenie czy active istneije 
         if(active){
+            //usuneicie klasy
             active.classList.remove('open');
            // active.classList.remove('open-active');
         }
+      //  jesli nie to dodanie klasyu
         this.classList.add('open');
+        //ustawienia active na this 
         active = this;
     }else{
+        //w przeciwnym wypatku toggle oraz ustawinie active 
         this.classList.toggle('open');
         active = this.classList.contains('open') ? this : null;
     }
