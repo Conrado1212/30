d2c8a1a6ac8f81beb497a93a67b343b69c6c28cb
Conrado1212 <conrado1212@gmail.com>
2025-02-24 19:15:05 +0100

    modal test

--- 5 - gallery/app/app.js
+++ 5 - gallery/app/app.js
@@ -1,10 +1,26 @@
 const panel = document.querySelectorAll('.panel');
-
+console.log(panel);
+let active = null;
 function toggle(){
     console.log('ddddddd');
-    this.classList.toggle('open');
+  
+
+    if(active !==this){
+        if(active){
+            active.classList.remove('open');
+           // active.classList.remove('open-active');
+        }
+        this.classList.add('open');
+        active = this;
+    }else{
+        this.classList.toggle('open');
+        active = this.classList.contains('open') ? this : null;
+    }
+
+
 }
 function toggleActive(e){
+    console.log(this);
     console.log(e.propertyName);
     if(e.propertyName.includes('flex')){
          this.classList.toggle('open-active');
