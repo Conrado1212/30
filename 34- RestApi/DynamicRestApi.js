f6abc2f93f453e3a7eae45d6119191b77e1b7703
Conrado1212 <conrado1212@gmail.com>
2025-03-23 17:38:51 +0100

    sort table

--- 34- RestApi/DynamicRestApi.js
+++ 34- RestApi/DynamicRestApi.js
@@ -3,13 +3,13 @@ const port = 3000;
 const app = express();
 
 const dynamic ={};
-
+//endpointy
 const endpoints =[];
-
+//id
 let id = 0;
-
+//tworzenie nowego endpointu 
 app.post('/new', (res,req)=>{
-
+    
     
  
 })
