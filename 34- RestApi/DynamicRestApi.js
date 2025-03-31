bc0b2660ad6ace1a3e22d350edf1f107c4955d22
Conrado1212 <conrado1212@gmail.com>
2025-03-31 19:14:55 +0200

    rest changes

--- 34- RestApi/DynamicRestApi.js
+++ 34- RestApi/DynamicRestApi.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 const path = require('path');
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'main')));
 app.use((req,res, next)=>{
     console.log('Request Headers:', req.headers);
     const resSend = res.send;
