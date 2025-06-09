b24378a798f7044a5bac042812bfdf43c6fdaa03
Conrado1212 <conrado1212@gmail.com>
2025-06-09 19:04:14 +0200

    rest changes

--- 34-RestApi/ExampleRest.js
+++ 34-RestApi/ExampleRest.js
@@ -5,20 +5,51 @@ const express = require("express");
 const port = process.env.PORT || 3001;
 
 
-//utworzenie instanci aplkikacji
+//utworzenie instancji aplikacji
 const app = express();
 
 //tworzeni endpoint get ktory zwrca przykladowo randoma liczbe 
 
 app.get(`/numbers`, (req,res) =>{
    const random =  Math.floor(Math.random() * 10);
-   //res.status(200) Ustawiamy kod statusu HTTP odpowiedzi na 200, co oznacza, że zapytanie zostało obsłużone poprawnie.
+   //res.status(200) Ustawiamy kod statusu HTTP odpowiedzi na 200
    res.status(200).json({
-    //res.json({ number: random }); Funkcja json() wysyła odpowiedź w formacie JSON. W tym przypadku przesyłany jest obiekt, który zawiera jeden klucz "number" z wartością wynikową przechowywaną w zmiennej random.
+    //res.json({ number: random }); Funkcja json() wysyła odpowiedź w formacie JSON
        number: random
    })
 })
 
+
+const Joi =  require('joi');
+let users = [{
+   id:1,name:'Jan Kowalski', email: 'test@gmail.com'
+}
+]
+app.post(`/user`, (req,res)=>{
+
+
+   const userSchema = Joi.object({
+      name: Joi.string().pattern(new RegExp('^[a-zA-Z0-9 ]*$')).min(3).max(30).required(),
+      email: Joi.string().email().required()
+   })
+
+   const {error} = userSchema.validate(req.body);
+
+   if(error){
+      return res.status(400).json({
+         error: error.details[0].message
+      })
+   }
+
+   const newUser = {
+      id: users.length+1,
+      name: req.body.name,
+      email: req.body.email,
+      // signatories: req.body.signatories
+   };
+   users.push(newUser);
+   res.status(201).json(newUser);
+})
 
 
 
@@ -40,6 +71,8 @@ app.get(`/numbers`, (req,res) =>{
 // http://localhost:${port} – adres do którego można się odwołać z tej samej maszyny,
 
 // http://192.168.0.50:${port} – przykładowy adres w sieci LAN, za pomocą którego aplikacja może być dostępna dla innych urządzeń w tej samej sieci.
+
+
 app.listen(port, () => {
     console.log(`App working at http://localhost:${port}`);
   });
