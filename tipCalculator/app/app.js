71f04da9d70fee2ad59b956fac5eaa2483cfef84
Conrado1212 <conrado1212@gmail.com>
2025-09-29 18:22:33 +0200

    tip calc

--- tipCalculator/app/app.js
+++ tipCalculator/app/app.js
@@ -1,5 +1,7 @@
 const billValue = document.getElementById('billValue');
 const peopleValue = document.getElementById('peopbleInp');
+const custom = document.getElementById('customBtn');
+const customValue = document.getElementById('customValue');
 const focus = document.getElementsByClassName('bill-value')[0];
 const focusPeople = document.getElementsByClassName('people-value')[0];
 const tip = document.querySelectorAll('.template button');
@@ -7,17 +9,26 @@ const tip = document.querySelectorAll('.template button');
 
 billValue.addEventListener('focus',()=>{
     focus.style.border = "2px solid var(--Green400)"
-})
+});
 
 billValue.addEventListener('blur',()=>{
     focus.style.border = "none"
-})
+});
 
 
 peopleValue.addEventListener('focus',()=>{
     focusPeople.style.border = "2px solid var(--Green400)"
-})
+});
 
 peopleValue.addEventListener('blur',()=>{
     focusPeople.style.border = "none"
-})
+});
+
+custom.addEventListener('click',()=>{
+    console.log('clicked');
+    custom.querySelector('label').remove()
+    customValue.style.display ="block";
+    customValue.focus();
+    custom.style.border = "2px solid var(--Green400)";
+    custom.classList.add('active');
+});
