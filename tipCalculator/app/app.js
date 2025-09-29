ec53a1db4d19f951b187f7b173ff9f46db119077
Conrado1212 <conrado1212@gmail.com>
2025-09-29 17:44:01 +0200

    tip calc

--- tipCalculator/app/app.js
+++ tipCalculator/app/app.js
@@ -1,3 +1,23 @@
 const billValue = document.getElementById('billValue');
-
+const peopleValue = document.getElementById('peopbleInp');
+const focus = document.getElementsByClassName('bill-value')[0];
+const focusPeople = document.getElementsByClassName('people-value')[0];
 const tip = document.querySelectorAll('.template button');
+
+
+billValue.addEventListener('focus',()=>{
+    focus.style.border = "2px solid var(--Green400)"
+})
+
+billValue.addEventListener('blur',()=>{
+    focus.style.border = "none"
+})
+
+
+peopleValue.addEventListener('focus',()=>{
+    focusPeople.style.border = "2px solid var(--Green400)"
+})
+
+peopleValue.addEventListener('blur',()=>{
+    focusPeople.style.border = "none"
+})
