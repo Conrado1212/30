b80ce9819d2769877c83f9074690d8ce2f95f0aa
Conrado1212 <conrado1212@gmail.com>
2025-11-03 19:27:37 +0100

    ticGame

---
+++ tictactoe/app/app.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.container div');
+
+const player =''
+buttons.forEach(button =>{
+    button.addEventListener('click',(e)=>{
+        console.log(e,'clicked');
+    })
+})
+  
