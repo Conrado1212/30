8ecf8e00b8760118e522168b737b0369801a3dd9
Conrado1212 <conrado1212@gmail.com>
2025-11-07 18:02:46 +0100

    toGame

---
+++ todoL/app/app.js
@@ -0,0 +1,10 @@
+const todoL = [];
+const add = document.getElementById("addTodo");
+const data = document.getElementById("task");
+
+
+add.addEventListener('click',(e)=>{
+    e.preventDefault();
+});
+
+
