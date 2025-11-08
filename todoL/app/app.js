9e7bce5bf1a4a560eb34b549686a899d45e741ab
Conrado1212 <conrado1212@gmail.com>
2025-11-08 14:17:44 +0100

    toGame

--- todoL/app/app.js
+++ todoL/app/app.js
@@ -1,11 +1,11 @@
-const todoL = [
+let todoL = [
     {   id: 1, text: "Zrobić zakupy", done: false },
         { id: 2, text: "Napisać raport", done: true }  
 ];
 const add = document.getElementById("addTodo");
 const container = document.querySelector('.container');
 const task = document.getElementById("task");
-const tasks = document.querySelectorAll('tasks');
+const tasks = document.querySelectorAll('.tasks');
 add.addEventListener('click',(e)=>{
     e.preventDefault();
     //console.log(e,"click");
@@ -60,16 +60,32 @@ function addTodo(text){
     const lastId = todoL.length >0 ? todoL[todoL.length-1].id : 0;
     const newTask = {
         id: lastId+1,
-        text: task.value.trim(),
+        text: text,
         done: false
     };
     todoL.push(newTask);   
 }
-function removetask(){
+container.addEventListener('click',(e) =>{
+    if(e.target.classList.contains('fa-trash')){
+        console.log('click closet');
+        const taskdiv = e.target.closest('.tasks');  
+        const id = taskdiv.querySelector('.info input').getAttribute('id');
+      //  console.log(id);
+    //    console.log(todoL);
+        removeTasks(id);
+        taskdiv.remove();
+    }
+})
 
+function removeTasks(id){
+    const nid = Number(id);
+ const index = todoL.findIndex(task=>task.id ==nid)
+ console.log('index',index);
+ if(index !== -1){
+     todoL.splice(index,1)
+ }
+ 
 }
-
-
 function editTask(){
 
 }
