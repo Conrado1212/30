3adc8c4db2fc68341385ab4c1a7e6ea1e7ade78b
Conrado1212 <conrado1212@gmail.com>
2025-11-08 10:49:06 +0100

    toGame

--- todoL/app/app.js
+++ todoL/app/app.js
@@ -20,20 +20,36 @@ function addTask(){
 
     const info = document.createElement("div");
     info.classList.add('info');
+    div.appendChild(info);
 
     const check = document.createElement("label");
     check.setAttribute("for", "check3");
+    info.appendChild(check);
 
     const input = document.createElement("input");
     input.setAttribute("type", "checkbox");
     input.setAttribute("id", "check3");
+    check.appendChild(input);
 
     const span = document.createElement("span");
-
+    check.appendChild(span);
     const p = document.createElement("p");
 
     p.classList.add("desc")
     p.textContent = "example";
+    info.appendChild(p);
+
+    const icon = document.createElement('div');
+    icon.classList.add('icon');
+    div.appendChild(icon);
+
+    const edit = document.createElement('i');
+    edit.classList.add('fa-solid', 'fa-pen-to-square');
+    icon.appendChild(edit);
+
+    const del = document.createElement('i');
+    del.classList.add('fa-solid', 'fa-trash');
+    icon.appendChild(del);
 }
 // //tworzyc cos takiego  
 // <div class="tasks">
@@ -56,5 +72,5 @@ function removetask(){
 
 
 function editTask(){
-    
+
 }
