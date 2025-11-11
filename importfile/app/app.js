79a47cb207a4ba0de4f8b655a412abfd4a342038
Conrado1212 <conrado1212@gmail.com>
2025-11-11 12:52:35 +0100

    filedrop

---
+++ importfile/app/app.js
@@ -0,0 +1,73 @@
+const file  = document.getElementById('file');
+const fileName  = document.getElementById('fileName');
+const cont  = document.getElementById('cont');
+
+
+function testFile(file){
+    return new Promise((resolve, reject)=>{
+        const reader = new FileReader();
+
+        reader.onload = (e) =>{
+            resolve(e.target.result)
+        }
+
+        reader.onerror = (error) =>{
+            reject(error);
+        }
+
+        reader.readAsBinaryString(file);
+    });
+}
+
+
+file.addEventListener('change',function(){
+    const file = this.files[0] ? this.files[0].name : 'no file'
+    fileName.textContent = file;
+    
+})
+
+
+cont.addEventListener('dragenter',()=>{
+    cont.classList.add('alt-border');
+    cont.classList.add('test');
+})
+cont.addEventListener('dragleave',()=>{
+    cont.classList.remove('alt-border');
+    cont.classList.remove('test');
+})
+
+
+
+cont.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+const reader = new FileReader();
+  cont.addEventListener('drop',(e)=>{
+      e.preventDefault();
+      const data = e.dataTransfer.files[0];
+      fileName.textContent = data.name;
+      reader.onload = function(e){
+          const div = document.createElement('div');
+          div.textContent = e.target.result;
+            cont.appendChild(div);
+      }
+     // reader.readAsDataURL(data);
+     reader.readAsText(data);
+     cont.classList.remove('alt-border');
+     cont.classList.remove('test');
+  })
+
+
+
+
+
+
+  function drop(event){
+    event.preventDefault();
+    //dane pliku
+  const data = event.dataTransfer.files[0];
+   // const reader = new FileReader();
+  //  reader.onload = function(e){
+        fileName.textContent = data.name;
+  //  }
+}
