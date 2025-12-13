0d1ed022aac5e7a9b2f4369d7081ee73dca6dcb8
Conrado1212 <conrado1212@gmail.com>
2025-12-13 19:27:54 +0100

    blog

--- BlogApp/style/app.js
+++ BlogApp/style/app.js
@@ -1,3 +1,9 @@
 const btns = document.querySelectorAll('.cat button');
-
-console.log(btns);
+const btns2 = [...btns].slice(0,-1);
+console.log(btns2);
+btns2.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        btns2.forEach(b =>b.classList.remove('active'));
+        btn.classList.add('active');
+    })
+})
