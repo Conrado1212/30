fb04907bfddab4140479f301f9854dd977bfc064
Conrado1212 <conrado1212@gmail.com>
2025-12-16 19:33:33 +0100

    blog

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -17,61 +17,71 @@ const blogArt = [{
   id: 1,
   title: "Step-by-step lifestyle management guide",
   desc: "his is a practical, human-first roadmap. Keep it realistic, iterate weekly, and measure progress with simple, visible signals.",
-  img:"images/life1.jpg"
+  img:"images/life1.jpg",
+  type: "Lifestyle"
 },
 {
   id: 2,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/start.jpg",
+  type: "Startup"
 },
 {
   id: 3,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/tech.jpg",
+  type: "Technology"
 },
 {
   id: 4,
-  title: "Building a clear roadmap for your startup success",
-  desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  title: "Unlocking the Full Potential of Your Apps",
+  desc: "From customization to cloud sync — here’s how to transform everyday software into powerful productivity tools.",
+  img:"images/tech1.jpg",
+  type: "Technology"
 },
 {
   id: 5,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/lif2.jpg",
+  type: "Lifestyle"
 },
 {
   id: 6,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/start2.jpg",
+  type: "Startup"
 },
 {
   id: 7,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/finance.jpg",
+  type: "Finance"
 },
 {
   id: 8,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/start3.jpg",
+  trype: "Startup"
 },
 {
   id: 9,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/life3.jpg",
+  type: "Lifestyle"
 },
 {
   id: 10,
   title: "Building a clear roadmap for your startup success",
   desc: "Creating an effective startup roadmap is about turning vision into structured, actionable steps. This guide walks you through.",
-  img:"images/start.jpg"
+  img:"images/tech2.jpg",
+  Type: "Technology"
 }
 ]
 
